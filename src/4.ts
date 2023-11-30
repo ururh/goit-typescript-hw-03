852f49b6dc28c8fdf5675f1052c5088bbf22213c
Inna Zlotska <innaz002001@gmail.com>
2023-11-30 22:52:52 +0200

    done tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,50 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = true;
+  protected key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      console.log(person.getKey().getSignature() );
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    } else {
+      this.door = false;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
